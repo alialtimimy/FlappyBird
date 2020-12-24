cd9e26d2fefa14bc4fe8714c08ea226559919870
alialtimimy <alialtimimy@gmail.com>
2020-12-24 05:16:52 -0500

    working on player

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -6,25 +6,34 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.player.Player;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch; //using sprite
-	Texture img; // texture
-	
+	private SpriteBatch batch; //using sprite
+	private Texture img; // texture
+	private Player player;
+	public static World world;
+
+
+
 	@Override
 	public void create () {
+		world = new World(new Vector2(0, 0), true);
 		batch = new SpriteBatch();
 		img = new Texture("Assets/Sprite_Sheets/Mobile - Flappy Bird - General Sprites0.png");
-
+		player = new Player(50, 256*3);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		world.step(1/60f,6,2);
 		batch.begin(); // when to start drawing
-		batch.draw(img, 0,0,145*4 + 4, 256*3);
+		player.update(batch);
 		batch.end(); // when to stop drawing
+
+
 	}
 	
 	@Override
--- core/src/com/mygdx/player/Player.java
+++ core/src/com/mygdx/player/Player.java
@@ -3,27 +3,43 @@ package com.mygdx.player;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+import static com.mygdx.game.Main.world;
 
 public class Player {
     private Texture img;
     private Sprite monkey;
     private SpriteBatch batch;
     private float x, y;
+    private Rectangle rect;
+    private Body body;
 
     public Player(){
         img = new Texture("Assets/icon.jpg");
         monkey = new Sprite(img);
         monkey.setSize(100,100);
+        createBody();
     }
-    public Player(float x, float y){
+    public Player(float x, float y ){
         this();
         this.x = x;
         this.y = y;
-
     }
 
     public void createBody(){
-        
+        monkey.setPosition(x, y);
+        rect = new Rectangle((int)monkey.getX(),(int)monkey.getY(),(int)monkey.getWidth(), (int)monkey.getHeight());
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody; //a body which moves is of type dynamic(static means doesnt move)
+        body = world.createBody(bodyDef);
+        FixtureDef fixtureDef = new FixtureDef();
+        PolygonShape polygonShape = new PolygonShape();
+        fixtureDef.shape = polygonShape;
+        polygonShape.setAsBox(rect.getX(), rect.getY());
+
     }
 
     public void moveBody(){
@@ -35,6 +51,20 @@ public class Player {
     }
 
     public void update(SpriteBatch batch){
+        monkey.setPosition(body.getPosition().x, body.getPosition().y);
+        render(batch);
 
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
     }
 }
